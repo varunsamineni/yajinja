bbb8e12958d4fb4d43dbe7b52152debc25205dc8
Dann Bohn <djb44@psu.edu>
2017-12-20 22:41:53 -0500

    add std_out option, test for extension of template file, and length of extension

--- jtpl/cli.py
+++ jtpl/cli.py
@@ -2,6 +2,7 @@ from jinja2 import Environment, FileSystemLoader, StrictUndefined, Undefined
 import yaml
 import click
 import os 
+import sys
 
 @click.command()
 @click.option('--input-file', '-i',
@@ -14,8 +15,16 @@ import os
     help="Allow Undefined Variables, Undefined variables evaluate to empty strings")
 @click.option('--output-file', '-o', default=None,
     help="If specified, ouput to this file, else we will output to input_file without the extension")
+@click.option('--std-out', '-s', is_flag=True,
+    help="don't write out template file, print to standard out instead")
 def main(input_file, template_file, environment,
-    undefined, output_file):
+    undefined, output_file, std_out):
+
+    if len(template_file.split('.')) < 2 and not output_file:
+        print('template file needs an extension if output file is not specified')
+        sys.exit(1)
+    else:
+        extension_len = len(template_file.split('.')[-1]) + 1
 
     if undefined:
         undefined = Undefined
@@ -31,13 +40,15 @@ def main(input_file, template_file, environment,
         env_variables = dict([(k, v) for k, v in os.environ.items()])
         input_variables = {**env_variables, **input_variables}
 
-    output_file = template_file[:-4]
+    if not output_file:
+        output_file = template_file[:-extension_len]
 
     rendered_template = template.render(input_variables)
-    print(rendered_template)
-
-    with open(output_file, 'w') as f:
-        f.write(rendered_template)
+    if std_out:
+        print(rendered_template)
+    else:
+        with open(output_file, 'w') as f:
+            f.write(rendered_template)
 
 if __name__ == '__main__':
     main()
