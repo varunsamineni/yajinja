792090b73c1fbccf4c1a8585005491f6022fb345
Dann Bohn <djb44@psu.edu>
2017-12-21 16:23:34 -0500

    only support modern python

--- setup.py
+++ setup.py
@@ -106,8 +106,6 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'Topic :: Utilities',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
