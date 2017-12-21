25906c7513d58967e4e46ef5a8ac4f0ee1f25c08
Dann Bohn <djb44@psu.edu>
2017-12-21 11:57:57 -0500

    add some tests

---
+++ tests/test_jtpl.py
@@ -0,0 +1,27 @@
+import sys
+from context import jtpl
+import os
+
+
+def test_env_vars_only():
+    vars = {'FOO':'ZING'}
+    os.environ.update(vars)
+    input_file = None
+    environment = True
+    r = jtpl.cli.process_variables(input_file, environment)
+    assert r.get('FOO') == 'ZING'
+
+def test_input_file_only():
+    input_file = './tests/test-input.yml'
+    environment = False
+    r = jtpl.cli.process_variables(input_file, environment)
+    assert r.get('baz') == 'bar'
+
+def test_env_and_input():
+    input_file = './tests/test-input.yml'
+    vars = {'FOO':'ZING'}
+    environment = True
+    os.environ.update(vars)
+    r = jtpl.cli.process_variables(input_file, environment)
+    assert r.get('FOO') == 'ZING'
+    assert r.get('baz') == 'bar'
